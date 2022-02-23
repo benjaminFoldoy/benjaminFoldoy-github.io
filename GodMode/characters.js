e9b0338269bc2cedd9ec3e4ec33b0c9a558fdac1
benjaminFoldoy <61045636+benjaminFoldoy@users.noreply.github.com>
2022-02-23 21:40:01 +0100

    GODMODE

---
+++ GodMode/characters.js
@@ -0,0 +1,44 @@
+class character{
+  constructor(nickname, color, initials){
+    this.nickname = nickname;
+    this.color = color;
+    this.initials = initials;
+  }
+}
+
+function reload_characters(){
+  characters.push(
+    new character(
+      "Steve",
+      "#0000ff",
+      "STV"
+    )
+  )
+
+  characters.push(
+    new character(
+      "Voice",
+      "#0000ff",
+      "UKS"
+    )
+  )
+
+  characters.push(
+    new character(
+      "Devin",
+      "#bb9900",
+      "DVN"
+    )
+  )
+
+  characters.push(
+    new character(
+      "Voice",
+      "#bb9900",
+      "UKD"
+    )
+  )
+
+}
+
+reload_characters();
